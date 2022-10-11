7829f4c25f88bc766cd4b2ddeb492be34567f268
Gera-MAJ <107657882+Gera-MAJ@users.noreply.github.com>
2022-10-10 23:33:26 -0300

    App de react desde cero

---
+++ src/components/AdiosMundo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AdiosMundo(){
+    return(
+        <>
+            <h4>Nos vemos a la salida</h4>
+        </>
+    )
+}
---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function HolaMundo() {
+
+    return(
+        <div>
+            <h1>Hola Mundo</h1>
+            <h2>Gerardo Jatip</h2>
+        </div>
+    )
+}
+
+export function AdiosMundo(){
+    return(
+        <div>
+            <h2>Nos vemos a la salida</h2>
+        </div>
+    )
+}
